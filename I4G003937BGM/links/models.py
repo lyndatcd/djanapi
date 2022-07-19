1875d20f66caa4eddc883d7416fa067f7eb604bf
lyndatcd <qvexvon@gmail.com>
2022-07-19 08:56:19 +0100

    first commit api

---
+++ I4G003937BGM/links/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+# Create your models here.
+
+class Link(models.Model):
+    target_url = models.URLField(max_length=200)
+    description = models.CharField(max_length=200)
+    identifier = models.SlugField(null=True, unique=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    active = models.BooleanField(default=True)
+    objects = models.Manager()
+    public = models.ActiveLinkManager()
+
+    def __str__(self):
+        return self.target_url

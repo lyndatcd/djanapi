8a9f541d46197b7c42c226ceca00c1b4a041600c
Adanna Eze <lyndatcd@users.noreply.github.com>
2022-08-04 10:56:42 +0100

    update views.py

--- I4G003937BGM/links/views.py
+++ I4G003937BGM/links/views.py
@@ -1,33 +1,57 @@
+from rest_framework import generics, serializers
+from .models import Link
+from .serializers import LinkSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from . import models
+from . import models 
 from . import serializers
-import datetime
-from django.utils import timezone
-
-
+import datetime 
 # Create your views here.
+
+
+class PostListApi(generics.ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostCreateApi(generics.CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDetailApi(generics.RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateApi(generics.UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDeleteApi(generics.DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+    
+    from django.utils import timezone
 
 class ActiveLinkView(APIView):
     """
     Returns a list of all active (publicly accessible) links
     """
-
     def get(self, request):
-        """
+        """ 
         Invoked whenever a HTTP GET Request is made to this view
         """
         qs = models.Link.public.all()
         data = serializers.LinkSerializer(qs, many=True).data
         return Response(data, status=status.HTTP_200_OK)
-
-
+    
 class RecentLinkView(APIView):
     """
     Returns a list of recently created active links
     """
-
     def get(self, request):
         """ 
         Invoked whenever a HTTP GET Request is made to this view
@@ -36,3 +60,4 @@ class RecentLinkView(APIView):
         qs = models.Link.public.filter(created_date__gte=seven_days_ago)
         data = serializers.LinkSerializer(qs, many=True).data
         return Response(data, status=status.HTTP_200_OK)
+    
